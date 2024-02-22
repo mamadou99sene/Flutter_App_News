d56d040aa25f4d4694038fd505f923327f2d451d
Mamadou <senemamadou1999@gmail.com>
2024-02-22 12:23:18 +0000

    ajout du model commentaire

---
+++ lib/Models/Commentaire.dart
@@ -0,0 +1,41 @@
+import 'package:news/Models/Article.dart';
+import 'package:news/Models/SousCommentaire.dart';
+import 'package:news/Models/Users.dart';
+
+class Commentaire {
+  late int id;
+  late String text;
+  late DateTime time;
+  late String author;
+  late int parent;
+  late Article article;
+  late Users user;
+  late List<SousCommentaire>? kids;
+  Commentaire(
+      {required this.id,
+      required this.text,
+      required this.time,
+      required this.article,
+      required this.parent,
+      required this.author,
+      required this.user,
+      this.kids});
+
+  Commentaire.FromJson(dynamic jsonData) {
+    id = jsonData["id"];
+    text = jsonData["text"];
+    time = jsonData["time"];
+    parent = jsonData["parent"];
+    author = jsonData["by"];
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "id": id,
+      "text": text,
+      "time": time,
+      "Art_id": article.id,
+      "Use_id": user.id,
+    };
+  }
+}
