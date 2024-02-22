4cd8515f009dfc1700c5cc0c57f0a1b983b14cb5
Mamadou <senemamadou1999@gmail.com>
2024-02-22 15:35:59 +0000

    recuperation des articles

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:news/Database/DatabaseHelper.dart';
+import 'package:news/Models/Article.dart';
 import 'package:news/Networking/HackerNewsAPI.dart';
 
 class Home extends StatelessWidget {
@@ -8,14 +9,78 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
         child: FutureBuilder(
-            future: HackerNewsAPI().getIDTopStories(),
+            future: HackerNewsAPI().getAllStories(),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
                 return SpinKitWave(
                   color: Colors.green,
                 );
               } else {
-                return Text(snapshot.data.toString());
+                List<Article>? listStories = snapshot.data;
+                return Container(
+                  decoration: BoxDecoration(
+                      // gradient: LinearGradient(colors: Colors.primaries)
+                      ),
+                  child: ListView.separated(
+                      separatorBuilder: (context, index) {
+                        return Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(
+                                      Icons.linked_camera,
+                                      color: Colors.green,
+                                    )),
+                                IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(
+                                      Icons.share,
+                                      color: Colors.green,
+                                    )),
+                                IconButton(
+                                    onPressed: () {
+                                      print(index);
+                                    },
+                                    icon: Icon(
+                                      Icons.comment,
+                                      color: Colors.green,
+                                    )),
+                              ],
+                            ),
+                            Divider(
+                              color: Colors.green,
+                              thickness: 1,
+                            )
+                          ],
+                        );
+                      },
+                      itemCount: (listStories == null ? 0 : listStories.length),
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          leading: CircleAvatar(
+                              radius: 40,
+                              backgroundImage: AssetImage("images/logo.jpg"),
+                              backgroundColor: Colors.green),
+                          title: Text(
+                            listStories![index].author,
+                            style: TextStyle(
+                                color: Colors.green,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          subtitle: Text(
+                            listStories[index].title,
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          selectedColor: Colors.grey,
+                          trailing: CircleAvatar(
+                              backgroundColor: Colors.green,
+                              child: Text(listStories[index].score.toString())),
+                        );
+                      }),
+                );
               }
             }));
   }
