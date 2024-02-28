a054ace2be2bfa918b033e634516ed193aad79cb
Mamadou <senemamadou1999@gmail.com>
2024-02-28 13:48:10 +0000

    ajout des bouttons partager, commentaire dans la page des commentaires

--- lib/Widgets/ShowComment.dart
+++ lib/Widgets/ShowComment.dart
@@ -5,7 +5,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:news/Models/Article.dart';
 import 'package:news/Models/Commentaire.dart';
+import 'package:news/Models/SousCommentaire.dart';
 import 'package:news/Networking/HackerNewsAPI.dart';
+import 'package:news/Widgets/ShowSousComment.dart';
+import 'package:share_plus/share_plus.dart';
 
 class ShowComment extends StatelessWidget {
   late Article currentArticle;
@@ -26,76 +29,186 @@ class ShowComment extends StatelessWidget {
                 );
               } else {
                 List<Commentaire>? listComment = snapshot.data;
+
                 return Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
+                    ListTile(
+                      leading: CircleAvatar(
+                        radius: 35,
+                        backgroundImage: AssetImage("images/logo.png"),
+                      ),
+                      title: Text(
+                        currentArticle.author,
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.w500),
+                      ),
+                      subtitle: Text(
+                        "${currentArticle.time.day}-${currentArticle.time.month}-${currentArticle.time.year} ${currentArticle.time.hour} ${currentArticle.time.minute}",
+                      ),
+                      trailing: CircleAvatar(
+                        backgroundColor: Colors.grey[200],
+                        child: IconButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            icon: Icon(
+                              Icons.close,
+                              color: Colors.black,
+                            )),
+                      ),
+                    ),
                     Container(
-                      height: 100,
                       margin: EdgeInsets.only(
-                          top: MediaQuery.of(context).size.width * 0.05,
+                          top: MediaQuery.of(context).size.width * 0.01,
                           left: MediaQuery.of(context).size.width * 0.01,
                           right: MediaQuery.of(context).size.width * 0.01),
                       padding: EdgeInsets.all(
                           MediaQuery.of(context).size.width * 0.05),
                       decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.grey),
-                      child: ListTile(
-                        leading: CircleAvatar(
-                          radius: 40,
-                          backgroundImage: AssetImage("images/logo.jpg"),
-                        ),
-                        title: Text(
-                          currentArticle.author,
-                          style: TextStyle(
-                              color: Colors.black, fontWeight: FontWeight.w500),
-                        ),
-                        subtitle: Text(
-                          currentArticle.title,
-                          style: TextStyle(color: Colors.black),
-                        ),
-                        selectedColor: Colors.grey,
-                        trailing: CircleAvatar(
-                          child: IconButton(
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                              icon: Icon(
-                                Icons.close,
-                                color: Colors.black,
-                              )),
-                        ),
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.grey[200]),
+                      child: Text(
+                        currentArticle.title,
+                        style: TextStyle(color: Colors.black, fontSize: 14),
                       ),
                     ),
-                    Text("${listComment!.length} commentaires"),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.favorite_outline,
+                              color: Colors.black,
+                            )),
+                        IconButton(
+                            onPressed: () {
+                              Share.share("Partger cet article");
+                            },
+                            icon: Icon(
+                              Icons.share,
+                            )),
+                        Row(
+                          children: [
+                            IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.comment_outlined,
+                                )),
+                            Text("${listComment!.length}")
+                          ],
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        TextButton(
+                          child: Text(
+                            "Commentaires",
+                            style: TextStyle(color: Colors.black, fontSize: 14),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
                     Expanded(
                       child: ListView.builder(
                           itemCount:
                               (listComment == null ? 0 : listComment.length),
                           itemBuilder: (context, index) {
-                            return Container(
-                                margin: EdgeInsets.only(
-                                    top: MediaQuery.of(context).size.width *
-                                        0.02,
-                                    left: MediaQuery.of(context).size.width *
-                                        0.01,
-                                    right: MediaQuery.of(context).size.width *
-                                        0.01),
-                                padding: EdgeInsets.all(
-                                    MediaQuery.of(context).size.width * 0.05),
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(15),
-                                    color: Colors.grey),
-                                child: ListTile(
-                                  leading: CircleAvatar(
-                                      backgroundImage:
-                                          AssetImage("images/logo.jpg")),
-                                  title: Text(listComment[index].author),
-                                  subtitle: Text(listComment[index].text),
-                                  trailing: Text(
-                                    "${listComment![index].time.day}-${listComment![index].time.month}-${listComment![index].time.year} ${listComment![index].time.hour} ${listComment![index].time.minute}",
-                                    style: TextStyle(color: Colors.black),
+                            return ListTile(
+                              leading: CircleAvatar(
+                                  backgroundImage:
+                                      AssetImage("images/logo.png")),
+                              subtitle: Column(
+                                children: [
+                                  Container(
+                                      margin: EdgeInsets.only(
+                                          top: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.02,
+                                          left: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.01,
+                                          right: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.01),
+                                      padding: EdgeInsets.all(
+                                          MediaQuery.of(context).size.width *
+                                              0.05),
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(15),
+                                          color: Colors.grey[200]),
+                                      child: Column(
+                                        children: [
+                                          Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                listComment![index].author,
+                                                style: TextStyle(
+                                                    color: Colors.black,
+                                                    fontWeight:
+                                                        FontWeight.w500),
+                                              ),
+                                            ],
+                                          ),
+                                          Text(
+                                            listComment[index].text,
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 14),
+                                          ),
+                                        ],
+                                      )),
+                                  Column(
+                                    children: [
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            "${listComment![index].time.day}-${listComment![index].time.month}-${listComment![index].time.year} ${listComment![index].time.hour} ${listComment![index].time.minute}",
+                                            style:
+                                                TextStyle(color: Colors.black),
+                                          ),
+                                        ],
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        children: [
+                                          TextButton(
+                                              onPressed: () {
+                                                Navigator.push(
+                                                    context,
+                                                    MaterialPageRoute(
+                                                        builder: (context) =>
+                                                            ShowSousComment(
+                                                                currentCommentaire:
+                                                                    listComment[
+                                                                        index])));
+                                              },
+                                              child: Text(
+                                                "Voir Reponses:",
+                                                style: TextStyle(
+                                                    color: Colors.black,
+                                                    fontSize: 14),
+                                              )),
+                                        ],
+                                      )
+                                    ],
                                   ),
-                                ));
+                                ],
+                              ),
+                            );
                           }),
                     )
                   ],
