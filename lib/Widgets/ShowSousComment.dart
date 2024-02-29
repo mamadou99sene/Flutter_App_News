94e896d0bf79972a05adc561aea85d1240efded4
Mamadou <senemamadou1999@gmail.com>
2024-02-29 22:38:28 +0000

    definition de la page sous commentaire

--- lib/Widgets/ShowSousComment.dart
+++ lib/Widgets/ShowSousComment.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:news/Models/Commentaire.dart';
+import 'package:news/Models/SousCommentaire.dart';
+import 'package:news/Networking/HackerNewsAPI.dart';
+import 'package:share_plus/share_plus.dart';
 
 class ShowSousComment extends StatelessWidget {
   late Commentaire currentCommentaire;
@@ -8,7 +12,189 @@ class ShowSousComment extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print(currentCommentaire.text);
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          ListTile(
+            leading: CircleAvatar(
+              radius: 35,
+              backgroundImage: AssetImage("images/logo.png"),
+            ),
+            title: Text(
+              currentCommentaire.author,
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.w500),
+            ),
+            subtitle: Text(
+              "${currentCommentaire.time.day}-${currentCommentaire.time.month}-${currentCommentaire.time.year} ${currentCommentaire.time.hour} ${currentCommentaire.time.minute}",
+            ),
+            trailing: CircleAvatar(
+              backgroundColor: Colors.grey[200],
+              child: IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(
+                    Icons.close,
+                    color: Colors.black,
+                  )),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(
+                top: MediaQuery.of(context).size.width * 0.01,
+                left: MediaQuery.of(context).size.width * 0.01,
+                right: MediaQuery.of(context).size.width * 0.01),
+            padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.05),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.grey[200]),
+            child: Text(
+              currentCommentaire.text,
+              style: TextStyle(color: Colors.black, fontSize: 14),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.favorite_outline,
+                    color: Colors.black,
+                  )),
+              IconButton(
+                  onPressed: () {
+                    Share.share("Partger cet article");
+                  },
+                  icon: Icon(
+                    Icons.share,
+                  )),
+              Row(
+                children: [
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.comment_outlined,
+                      )),
+                ],
+              ),
+            ],
+          ),
+          FutureBuilder(
+              future:
+                  HackerNewsAPI().getSousCommentaires(currentCommentaire.id),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return SpinKitCircle(
+                    color: Colors.black,
+                  );
+                } else {
+                  List<SousCommentaire>? lisSouCommentaire = snapshot.data;
+                  return Expanded(
+                    child: ListView.builder(
+                        itemCount: (lisSouCommentaire == null
+                            ? 0
+                            : lisSouCommentaire.length),
+                        itemBuilder: (context, index) {
+                          return Container(
+                            margin: EdgeInsets.only(
+                              left: MediaQuery.of(context).size.width * 0.05,
+                            ),
+                            padding: EdgeInsets.all(
+                                MediaQuery.of(context).size.width * 0.05),
+                            child: ListTile(
+                              leading: CircleAvatar(
+                                  radius: 20,
+                                  backgroundImage:
+                                      AssetImage("images/logo.png")),
+                              subtitle: Column(
+                                children: [
+                                  Container(
+                                      margin: EdgeInsets.only(
+                                          top: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.02,
+                                          left: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.01,
+                                          right: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.01),
+                                      padding: EdgeInsets.all(
+                                          MediaQuery.of(context).size.width *
+                                              0.05),
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(15),
+                                          color: Colors.grey[200]),
+                                      child: Column(
+                                        children: [
+                                          Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                lisSouCommentaire![index]
+                                                    .author,
+                                                style: TextStyle(
+                                                    color: Colors.black,
+                                                    fontWeight:
+                                                        FontWeight.w500),
+                                              ),
+                                            ],
+                                          ),
+                                          Text(
+                                            lisSouCommentaire[index].text,
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 14),
+                                          ),
+                                        ],
+                                      )),
+                                  Column(
+                                    children: [
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            "${lisSouCommentaire![index].time.day}-${lisSouCommentaire![index].time.month}-${lisSouCommentaire![index].time.year} ${lisSouCommentaire![index].time.hour} ${lisSouCommentaire![index].time.minute}",
+                                            style:
+                                                TextStyle(color: Colors.black),
+                                          ),
+                                        ],
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        children: [
+                                          TextButton(
+                                              onPressed: () {},
+                                              child: Text(
+                                                "Voir Reponses:",
+                                                style: TextStyle(
+                                                    color: Colors.black,
+                                                    fontSize: 14),
+                                              )),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        }),
+                  );
+                }
+              }),
+        ],
+      ),
+    );
   }
 }
