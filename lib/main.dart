b1b730bbb0274ebf43d8a51b4aace1e966bbe8dc
Mamadou <senemamadou1999@gmail.com>
2024-03-03 21:59:31 +0000

    injection de dependance get_it pour instancier la base de données comme singleton

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:news/App.dart';
+import 'package:get_it/get_it.dart';
+import 'package:news/Database/DatabaseHelper.dart';
 
 void main() {
+  GetIt.instance.registerLazySingleton<DatabaseHelper>(
+    () => new DatabaseHelper(),
+  );
   runApp(MaterialApp(
     home: App(),
   ));
