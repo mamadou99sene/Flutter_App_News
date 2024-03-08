6b1369f1602f9af8f1adb424fe1e77d298a83f46
Mamadou <senemamadou1999@gmail.com>
2024-03-08 23:11:30 +0000

    ajout du appbar

--- lib/App.dart
+++ lib/App.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:news/Providers/NewsProvider.dart';
 import 'package:news/Screens/Home.dart';
+import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
-  const App({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Home(),
+    return Material(
+      child: ChangeNotifierProvider(
+        create: (context) => NewsProvider(),
+        child: Scaffold(
+          appBar: AppBar(
+            actions: [
+              Consumer<NewsProvider>(builder: (context, value, child) {
+                if (!value.lenSelectedSubOne) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.restore_from_trash_outlined),
+                        color: Colors.white,
+                      ),
+                      Text(
+                        "${value.selectedStories.length}",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w900, fontSize: 20),
+                      )
+                    ],
+                  );
+                } else {
+                  print("variable bool ${value.lenSelectedSubOne}");
+                }
+                return Container();
+              })
+            ],
+          ),
+          body: Home(),
+        ),
+      ),
     );
   }
 }
