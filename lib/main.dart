8dad5fc9f86eb7f04cf5e77823a5a2784069c7ec
Mamadou <senemamadou1999@gmail.com>
2024-03-09 00:52:27 +0000

    Routine de nettoyage periodique

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:news/App.dart';
 import 'package:get_it/get_it.dart';
 import 'package:news/Database/DatabaseHelper.dart';
+import 'package:news/Networking/ArticleService.dart';
 
 void main() {
   GetIt.instance.registerLazySingleton<DatabaseHelper>(
     () => new DatabaseHelper(),
   );
+  ArticleService service = ArticleService();
+  service.startCleanupRoutine();
   runApp(MaterialApp(
     home: App(),
   ));
