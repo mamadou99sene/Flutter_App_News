f47c16f6c2edf3f1a9d80a8a9a5cd8204ba5f9e9
Mamadou <senemamadou1999@gmail.com>
2024-03-09 22:50:45 +0000

    ajout des methode onloadtorie, onLoadComment, onLoadSubComment et la recuperation periodique des articles

--- lib/Networking/ArticleService.dart
+++ lib/Networking/ArticleService.dart
@@ -1,10 +1,15 @@
 import 'dart:async';
+import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
 import 'package:news/Database/DatabaseHelper.dart';
 import 'package:news/Models/Article.dart';
+import 'package:news/Networking/HackerNewsAPI.dart';
+
+import '../Models/Commentaire.dart';
+import '../Models/SousCommentaire.dart';
 
 class ArticleService {
-  final DatabaseHelper _databaseHelper = DatabaseHelper();
+  final DatabaseHelper _databaseHelper = GetIt.instance<DatabaseHelper>();
 
   Future<void> cleanupNonFavoriteArticles() async {
     List<Article> nonFavoriteArticles =
@@ -44,5 +49,79 @@ class ArticleService {
       await cleanupNonFavoriteArticles();
       print('Routine de nettoyage exécutée.');
     });
+  }
+
+  Future<List<Article>> onLoadStories({bool forceToAPI = false}) async {
+    List<Article> localArticles = await _databaseHelper.getAllStories();
+    if (forceToAPI || localArticles.isEmpty) {
+      List<Article> apiArticles = await HackerNewsAPI().getAllStories();
+      for (Article article in apiArticles) {
+        try {
+          await _databaseHelper.insertStories(article);
+          print("insertion de nouveau article");
+        } catch (e) {
+          await _databaseHelper.UpdateStorie(article);
+          print("modification de l'article num ${article.id}");
+        }
+      }
+      return apiArticles;
+    }
+    return localArticles;
+  }
+
+  void startPeriodicStoriesRecover() {
+    const Duration period =
+        const Duration(minutes: 5); // Intervalles de 5 minutes
+    Timer.periodic(period, (Timer timer) async {
+      await onLoadStories(forceToAPI: true);
+      print('Recuperation périodique des aricles lancée');
+    });
+  }
+
+  Future<List<Commentaire>> onLoadCommentaires(Article article) async {
+    List<Commentaire> localCommentaires =
+        await _databaseHelper.getCommentsByArticleId(article);
+
+    if (localCommentaires.isEmpty) {
+      List<Commentaire> apiCommentaires =
+          await HackerNewsAPI().getCommentsBeforeRecoverIds(article.id);
+      print("recuperation depuis l'API");
+
+      /*for (Commentaire commentaire in apiCommentaires) {
+        try {
+          await _databaseHelper.insertCommentaire(commentaire);
+          print("Insertion de nouveau de commentaire");
+        } catch (e) {
+          await _databaseHelper.updateCommentaire(commentaire);
+        }
+      }*/
+
+      return apiCommentaires;
+    }
+    return localCommentaires;
+  }
+
+  Future<List<SousCommentaire>> onLoadSousCommentaires(
+      Commentaire commentaire) async {
+    List<SousCommentaire> localSousCommentaires =
+        await _databaseHelper.getSubCommentsByCommentId(commentaire);
+
+    if (localSousCommentaires.isNotEmpty) {
+      return localSousCommentaires;
+    } else {
+      List<SousCommentaire> apiSousCommentaires =
+          await HackerNewsAPI().getSousCommentaires(commentaire.id);
+
+      /*for (SousCommentaire sousCommentaire in apiSousCommentaires) {
+        try {
+          await _databaseHelper.insertSousCommentaire(sousCommentaire);
+          print("Insertion de nouveau de sous commentaire");
+        } catch (e) {
+          await _databaseHelper.updateSousCommentaire(sousCommentaire);
+        }
+      }*/
+
+      return apiSousCommentaires;
+    }
   }
 }
